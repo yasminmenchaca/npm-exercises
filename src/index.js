8839ebf760785fed8753bb443405c6b309cb8559
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-22 09:38:03 -0600

    -

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+const $ = require('jquery');
+
 const sayHello = (name) => {
   return 'Hello ' + name;
 };
 
 console.log(sayHello("Yasmin"));
+
+$(document).ready(function(){
+ $("#message").html("This is Hello World by JQuery");
+});
